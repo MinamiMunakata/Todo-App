39f8d8170dadf8fa3c8a2495c7418ad01e87885d
Minami Munakata <june28munazo@gmail.com>
2018-10-09 14:35:36 -0700

    create TodoTableViewCell

---
+++ ToDo App/Todo.swift
@@ -0,0 +1,20 @@
+//
+//  Todo.swift
+//  ToDo App
+//
+//  Created by minami on 2018-10-09.
+//  Copyright © 2018 宗像三奈美. All rights reserved.
+//
+
+import Foundation
+
+struct Todo {
+    var title: String
+    var todoDescription: String
+    var priority: Int
+    var isCompleted: Bool
+    
+    
+    
+    
+}
---
+++ ToDo App/TodoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoTableViewCell.swift
+//  ToDo App
+//
+//  Created by minami on 2018-10-09.
+//  Copyright © 2018 宗像三奈美. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
